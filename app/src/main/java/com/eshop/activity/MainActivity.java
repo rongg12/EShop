a50437128835990e934cba139b0a8865dd316706
wangli <1019480454@qq.com>
2017-05-18 12:13:26 +0800

    第一次提交

--- app/src/main/java/com/eshop/activity/MainActivity.java
+++ app/src/main/java/com/eshop/activity/MainActivity.java
@@ -12,4 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public void onContentChanged() {
+        super.onContentChanged();
+    }
 }
