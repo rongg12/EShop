31c05273af6a6b37212c22afaa3e9d9a08e2529c
wangli <1019480454@qq.com>
2017-05-18 16:50:34 +0800

    资源创建

---
+++ app/src/main/java/com/eshop/activity/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.eshop.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.eshop.R;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
